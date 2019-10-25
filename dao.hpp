fa45460279377b76ff831022c0cbb1930989857b
xmh0511 <970252187@qq.com>
2019-10-25 10:59:43 +0800

    update dao

--- dao.hpp
+++ dao.hpp
@@ -1,41 +1,37 @@
 #pragma once
 #include "simple_pool.hpp"
 #include <atomic>
+#include <utility>
 namespace xorm {
-	template<typename T>
-	struct Pool {
-		simple_pool<T> pool_;
-		std::atomic_bool is_init = false;
-	};
 	struct dataBaseConfig {
 		std::string host;
 		std::string user;
 		std::string password;
 		std::string dbname;
 		unsigned int port;
+		std::size_t conn_number;
+	};
+
+	template<typename T>
+	struct Pool {
+		Pool(std::size_t size, dataBaseConfig const& config):pool_(size){
+			pool_.init_pool([config](auto& iter) {
+				iter = std::make_shared<T>(config.host, config.user, config.password, config.dbname, config.port);
+			});
+		}
+		simple_pool<T> pool_;
 		std::atomic_bool is_init = false;
 	};
+	inline dataBaseConfig& init_database_config(dataBaseConfig const& v = {}) {
+		static dataBaseConfig config{ v };
+		return config;
+	}
 	template<typename DataBaseType>
 	class dao {
-	public:
-		static dataBaseConfig& init_config(dataBaseConfig const& v = {}) {
-			static dataBaseConfig config;
-			if (!config.is_init) {
-				config = v;
-				config.is_init = true;
-			}
-			return config;
-		}
-		static simple_pool<DataBaseType>& get_conn_pool(std::size_t size = 0) {
-			static Pool<DataBaseType> pool;
-			if (!pool.is_init) {
-				pool.pool_.expand(size);
-				auto& config = init_config();
-				pool.pool_.init_pool([&config](auto& iter) {
-					iter->connect(config.host, config.user, config.password,config.dbname, config.port);
-				})
-				pool.is_init = true;
-			}
+	private:
+		static simple_pool<DataBaseType>& get_conn_pool() {
+			auto& config = init_database_config();
+			static Pool<DataBaseType> pool{ config.conn_number,config };
 			return pool.pool_;
 		}
 	public:
@@ -44,7 +40,38 @@ namespace xorm {
 			conn_ = pool.takeout();
 		}
 	public:
-
+		/*return type   
+		  first:effective row number
+		  second: insert data's id(key)
+		*/
+		template<typename T> 
+		std::pair<std::int64_t, std::int64_t> insert(T&& t) {
+			if (!conn_->is_connect()) {
+				return {0,0};
+			}
+			return conn_->insert(std::forward<T>(t));
+		}
+		template<typename T>
+		bool del(std::string const& condition) {
+			if (!conn_->is_connect()) {
+				return false;
+			}
+			return conn_->del<T>(condition);
+		}
+		template<typename T>
+		bool update(T&& v, std::string const& condition = "") {
+			if (!conn_->is_connect()) {
+				return false ;
+			}
+			return conn_->update(std::forward<T>(v), condition);
+		}
+		template<typename T>
+		std::pair<bool, std::vector<T>> query(std::string const& condition = "") {
+			if (!conn_->is_connect()) {
+				return { false,{} };
+			}
+			return conn_->query<T>(condition);
+		}
 	public:
 		~dao() {
 			simple_pool<DataBaseType>& pool = get_conn_pool();
