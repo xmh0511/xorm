96a07f0ef8f2543fdc17bec86352bebf017993e9
xmh0511 <970252187@qq.com>
2019-10-25 17:27:55 +0800

    update

--- main.cpp
+++ main.cpp
@@ -21,32 +21,25 @@ int main() {
 	config.user = "root";
 	init_database_config(config);
 
-	dao_t<mysql> dao;
-	test data;
-	data.id = 0;
-	data.a.clear();
-	data.b = "t0 中国好" + std::to_string(5);
-	data.time = "2019-10-25 18:05:01";
-	data.date = "2019-10-09";
-	data.tm.format_timestamp(std::time(nullptr));
-	dao.insert(data);
 
 	auto t0 = std::thread([]() {
 		for (auto i = 0; i < 5; i++) {
 			dao_t<mysql> dao;
+			dao.start_transaction();
 			test data;
 			data.id = 0;
 			data.a = i;
 			data.b = "t0 中国好"+ std::to_string(i);
 			data.time = "2019-10-25 18:05:01";
 			data.date = "2019-10-09";
-			data.tm = "18:19:01";
+			data.tm.format_timestamp(std::time(nullptr));
 			dao.insert(data);
 			std::this_thread::sleep_for(std::chrono::milliseconds(800));
 		}
 	});
 	auto t1 = std::thread([]() {
 		dao_t<mysql> t;
+		t.start_transaction();
 		for (auto i = 0; i < 5; i++) {
 			test data;
 			data.id = 0;
@@ -54,13 +47,14 @@ int main() {
 			data.b = "t1 中国好" + std::to_string(i);
 			data.time = "2019-10-25 18:05:01";
 			data.date = "2019-10-09";
-			data.tm = "18:19:01";
+			data.tm.format_timestamp(std::time(nullptr));
 			t.insert(data);
 			std::this_thread::sleep_for(std::chrono::milliseconds(802));
 		}
 	});
 	auto t2 = std::thread([]() {
 		dao_t<mysql> t;
+		t.start_transaction();
 		for (auto i = 0; i < 5; i++) {
 			test data;
 			data.id = 0;
@@ -68,13 +62,14 @@ int main() {
 			data.b = "t2 中国好" + std::to_string(i);
 			data.time = "2019-10-25 18:05:01";
 			data.date = "2019-10-09";
-			data.tm = "18:19:01";
+			data.tm.format_timestamp(std::time(nullptr));
 			t.insert(data);
 			std::this_thread::sleep_for(std::chrono::milliseconds(803));
 		}
 	});
 	auto t3 = std::thread([]() {
 		dao_t<mysql> t;
+		t.start_transaction();
 		for (auto i = 0; i < 5; i++) {
 			test data;
 			data.id = 0;
@@ -82,14 +77,20 @@ int main() {
 			data.b = "t3 中国好" + std::to_string(i);
 			data.time = "2019-10-25 18:05:01";
 			data.date = "2019-10-09";
-			data.tm = "18:19:01";
+			data.tm.format_timestamp(std::time(nullptr));
 			t.insert(data);
 			std::this_thread::sleep_for(std::chrono::milliseconds(804));
 		}
 	});
+	{
+		dao_t<mysql> dao_query;
+		dao_query.start_transaction();
+		auto r = dao_query.query<test>(" for update");
+	}
 	t0.join();
 	t1.join();
 	t2.join();
 	t3.join();
 	std::cout << "end" << std::endl;
+	std::cin.get();
 }
